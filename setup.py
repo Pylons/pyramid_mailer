0508cac193901b84931fe85b2a205a369916f71e
Dan Jacob <danjac354@gmail.com>
2011-02-25 19:14:22 +0000

    version updated

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ from setuptools import setup
 
 setup(
     name='pyramid_mailer',
-    version='0.2.4',
+    version='0.3',
     license='BSD',
     author='Dan Jacob',
     author_email='danjac354@gmail.com',
