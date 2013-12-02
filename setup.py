fc5b41ef085557af63fc59119ff75ba8eb8a5216
Florian Mueller <[email]>
2013-12-02 11:44:09 +0100

    Set version to 0.13-hp

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ testing_extras = tests_require + [
 
 setup(
     name='pyramid_mailer',
-    version='0.13',
+    version='0.13-hp',
     license='BSD',
     author='Dan Jacob',
     author_email='danjac354@gmail.com',
