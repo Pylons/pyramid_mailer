29968d663883c589aaedec3fe1e6cd41b7eb1362
Tres Seaver <tseaver@palladion.com>
2014-12-10 14:01:36 -0500

    Move overview docs into README.rst.
    Make '--long-description' incorporate README.rst, CHANGES.rst.

--- README.rst
+++ README.rst
@@ -1,8 +1,32 @@
+``pyramid_mailer``
+==================
+
 pyramid_mailer is a package for sending email from your Pyramid application.
-It is compatible with Python 2.6. 2.7 and 3.2.
+It is compatible with Python 2.6. 2.7, 3.2, 3.3, and 3.4, as well as PyPy
+and PyPy3.
 
-See the documentation at
-http://docs.pylonsproject.org/projects/pyramid_mailer/en/latest/ for more info.
+This package includes:
 
-pyramid_mailer uses code from the Lamson Project (http://lamsonproject.org/)
-with permission.  See the LICENSE.txt file for more information.
+1. Wrapping the low-level Python ``email`` library with an easy-to-use
+   API, which includes attachments and mulipart content.
+
+2. Send emails immediately or to add to a maildir queue.
+
+3. Managing email sends inside a transaction, to prevent emails being sent
+   if your code raises an exception.
+
+4. Features to help with unit testing.
+
+``pyramid_mailer`` uses the ``repoze.sendmail`` library for managing email
+sending and transacton management, and borrows code (with permission) from
+Zed Shaw's ``lamson`` for wrapping email messages (http://lamsonproject.org/).
+See the LICENSE.txt file for more information.
+
+Links
+-----
+
+- `documentation
+  <http://docs.pylonsproject.org/projects/pyramid_mailer/en/latest/>`_
+
+- `development version
+  <https://github.com/Pylons/pyramid_mailer>`_
--- setup.py
+++ setup.py
@@ -1,33 +1,3 @@
-"""
-pyramid_mailer is a package for taking the pain out of sending emails in your
-Pyramid project.  It is compatible with Python 2.5, 2.6, 2.7, and 3.2.
-
-This includes:
-
-1. Wrapping the low-level Python email library with an easy-to-use
-   API, which includes attachments and mulipart content.
-
-2. Send emails immediately or to add to a maildir queue.
-
-3. Managing email sends inside a transaction, to prevent emails being sent
-   if your code fails.
-
-4. Features to help with unit testing.
-
-pyramid_mailer uses the repoze_sendmail library for managing email sending
-and transacton management, and borrows code from Zed Shaw's Lamson for
-wrapping email messages.
-
-Links
-`````
-
-* `documentation
-  <http://docs.pylonsproject.org/projects/pyramid_mailer/en/latest/>`_
-* `development version
-  <https://github.com/Pylons/pyramid_mailer>`_
-
-"""
-
 from setuptools import setup
 
 docs_extras = [
@@ -43,14 +13,20 @@ testing_extras = tests_require + [
     'coverage',
     ]
 
+with open('README.rst') as f:
+    README = f.read()
+
+with open('CHANGES.rst') as f:
+    CHANGES = f.read()
+
 setup(
     name='pyramid_mailer',
-    version='0.13',
+    version='0.14dev',
     license='BSD',
     author='Dan Jacob',
     author_email='danjac354@gmail.com',
     description='Sendmail package for Pyramid',
-    long_description=__doc__,
+    long_description='\n\n'.join([README, CHANGES]),
     url="http://docs.pylonsproject.org/projects/pyramid_mailer/en/latest/",
     packages=[
         'pyramid_mailer',
